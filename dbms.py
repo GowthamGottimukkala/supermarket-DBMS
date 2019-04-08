1320583cc3803b2439acc0afae6e9d4873750618
Shravan1106 <shravankumar2809@gmail.com>
2019-04-08 18:11:14 +0530

    second

--- dbms.py
+++ dbms.py
@@ -1,11 +1,18 @@
 import wx 
 
+ID_MENU_PRODUCT = wx.NewId()
+ID_MENU_CUSTOMER = wx.NewId()
+ID_MENU_EMPLOY = wx.NewId()
+ID_MENU_PAYMENT = wx.NewId()
+
+
 
 
 class SuperMarket(wx.Frame):
 	def __init__(self,*args, **kwargs):
 		super(SuperMarket,self).__init__(*args, **kwargs)
-		
+		panel = wx.Panel(self)
+		panel.SetBackgroundColour('grey')
 		self.Mbar()
 	
 	def Mbar(self):
@@ -38,7 +45,7 @@ class SuperMarket(wx.Frame):
 		editMenu.AppendSeparator()
 		editMenu.Append(wx.ID_SELECTALL,'Select All')
 		editMenu.Append(wx.ID_PREFERENCES,'Preferences')
-	       
+		   
 		#view menu
 		viewMenu=wx.Menu()
 		self.shtl=viewMenu.Append(wx.ID_ANY,'Show ToolBar',kind=wx.ITEM_CHECK)
@@ -56,18 +63,93 @@ class SuperMarket(wx.Frame):
 		self.statusbar=self.CreateStatusBar()
 		self.statusbar.SetStatusText('Ready')
 		
-				
+		#tables
+		tableMenu=wx.Menu()
+		self.shpl=tableMenu.Append(wx.ID_ANY,'Product',kind=wx.ITEM_CHECK)
+		self.shcl=tableMenu.Append(wx.ID_ANY,'Customer',kind=wx.ITEM_CHECK)
+		self.shel=tableMenu.Append(wx.ID_ANY,'Employees',kind=wx.ITEM_CHECK)
+		self.shpal=tableMenu.Append(wx.ID_ANY,'Paymemt',kind=wx.ITEM_CHECK)
+
+		tableMenu.Check(self.shpl.GetId(),True)
+		tableMenu.Check(self.shcl.GetId(),False)
+		tableMenu.Check(self.shel.GetId(),False)
+		tableMenu.Check(self.shpal.GetId(),False)
+
+		self.Bind(wx.EVT_MENU,self.Onclicked1,self.shpl)
+		self.Bind(wx.EVT_MENU,self.Onclicked2,self.shcl)		
+		self.Bind(wx.EVT_MENU,self.Onclicked3,self.shel)
+		self.Bind(wx.EVT_MENU,self.Onclicked4,self.shpal)		
+
 		menubar.Append(fileMenu,'&File')
 		menubar.Append(editMenu, '&Edit')
 		menubar.Append(viewMenu,'&View')
+		menubar.Append(tableMenu,'&Tables')
 		self.SetMenuBar(menubar)
-		
-		
 		
 		self.SetSize((500,500))
 		self.SetTitle('Super Market')
 		self.Center()
-		
+
+	def Onclicked1(self,e):
+		if self.shpl.IsChecked():
+			self.shpl.Show()
+			self.shcl.Hide()
+			self.shel.Hide()
+			self.shpal.Hide()
+
+		vbox = wx.BoxSizer(wx.HORIZONTAL)
+		btn1 = wx.Button(panel, label='Insert', size=(7, 3))
+		btn2 = wx.Button(panel, label='Update', size=(7, 3))
+		btn3 = wx.Button(panel, label='Delete', size=(7, 3))
+		vbox.Add(btn1, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn2, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn3, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		panel.SetSizer(vbox)
+	def Onclicked2(self,e):
+	   	if self.shcl.IsChecked():
+			self.shpl.Hide()
+			self.shcl.Show()
+			self.shel.Hide()
+			self.shpal.Hide()
+
+		vbox = wx.BoxSizer(wx.HORIZONTAL)
+		btn1 = wx.Button(panel, label='Insert', size=(7, 3))
+		btn2 = wx.Button(panel, label='Update', size=(7, 3))
+		btn3 = wx.Button(panel, label='Delete', size=(7, 3))
+		vbox.Add(btn1, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn2, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn3, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		panel.SetSizer(vbox)
+	def Onclicked3(self,e):
+	   	if self.shel.IsChecked():
+			self.shpl.Hide()
+			self.shcl.Hide()
+			self.shel.Show()
+			self.shpal.Hide()
+
+		vbox = wx.BoxSizer(wx.HORIZONTAL)
+		btn1 = wx.Button(panel, label='Insert', size=(7, 3))
+		btn2 = wx.Button(panel, label='Update', size=(7, 3))
+		btn3 = wx.Button(panel, label='Delete', size=(7, 3))
+		vbox.Add(btn1, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn2, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn3, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		panel.SetSizer(vbox)
+	def Onclicked4(self,e):
+	   	if self.shpal.IsChecked():
+			self.shpl.Hide()
+			self.shcl.Hide()
+			self.shel.Hide()
+			self.shpal.Show()
+
+		vbox = wx.BoxSizer(wx.HORIZONTAL)
+		btn1 = wx.Button(panel, label='Insert', size=(7, 3))
+		btn2 = wx.Button(panel, label='Update', size=(7, 3))
+		btn3 = wx.Button(panel, label='Delete', size=(7, 3))
+		vbox.Add(btn1, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn2, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		vbox.Add(btn3, wx.ID_ANY, wx.EXPAND | wx.ALL, border=1)
+		panel.SetSizer(vbox)
 	def OnQuit(self,e):
 		self.Close()
 		
@@ -90,6 +172,5 @@ def main():
 	app.MainLoop()
 	
 if __name__ == '__main__':
-	main()
-	
+	main()	
 		
